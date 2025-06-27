8c5518b7e9caca5552bfea939fa67e256bb84449
charanaryan <149886275+charanaryan@users.noreply.github.com>
2025-06-27 14:05:20 +0530

    Fix: Case-sensitive component imports

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import ProductList from '../components/ProductList';
+import ProductList from '../components/productList';
 import ProductForm from '../components/productForm';
 
 
